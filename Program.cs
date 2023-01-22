473ca7279c2bda7e355474e75025e8a1b5e7f4a5
toylep <toylenium@mail.ru>
2023-01-22 11:26:14 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+internal class Program
+{
+   private static ITelegramBotClient bot;
+    private static string token { get; set; } = "5878539084:AAGTLi_RIuPYsFpesxhT2ZdcyCmuxjU0eGg";
+    
+    
+    private static void Main(string[] args)
+    {
+        bot = new TelegramBotClient(token);
+        bot.OnMessage += OnMessageHandler;
+      /*  Console.WriteLine("Bot started "+ bot.GetMeAsync().Result.FirstName);
+        var cts = new CancellationTokenSource();
+        var cancellationToken = cts.Token;
+        var receiverOptions = new ReceiverOptions
+        {
+            AllowedUpdates = { },
+        };
+
+        var me = bot.GetMeAsync();
+        Console.WriteLine($"HEllo my name is {me}");
+        Console.ReadLine();
+      */
+
+    }
+    private static void OnMessageHandler(object sender, MessageEventArgs e)
+}
